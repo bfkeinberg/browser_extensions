01bc0e34bf76cbddd723e519bfa0df86ce83e4e1
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2022-06-04 07:13:58 -0700

    Enable show weather provider for Firefox extension

--- firefox_rp_extension/background-script.js
+++ firefox_rp_extension/background-script.js
@@ -1,6 +1,6 @@
  const handleClick = (tab) => {
     const pathParams = new URL(tab.url).pathname.split('/');
-    browser.tabs.create({url: `https://www.randoplan.com?rwgpsRoute=${pathParams[2]}&stopAfterLoad=true`});
+    browser.tabs.create({url: `https://www.randoplan.com?rwgpsRoute=${pathParams[2]}&showProvider=true&stopAfterLoad=true`});
  };
 
  browser.pageAction.onClicked.addListener(handleClick);
