3b63fe73258f4647fbcedf77079a8bac769fb019
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-01-25 20:05:29 -0800

    Update Chrome extension for Strava route support

--- chrome_rp_extension/background.js
+++ chrome_rp_extension/background.js
@@ -9,6 +9,9 @@ chrome.runtime.onInstalled.addListener(() => {
             conditions: [
                 new chrome.declarativeContent.PageStateMatcher({
                     pageUrl: { hostEquals: 'ridewithgps.com', pathContains: 'routes', schemes: ['https'] },
+                }),
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: { hostSuffix: 'strava.com', pathContains: 'routes', schemes: ['https'] },
                 })
             ],
             actions: [new chrome.declarativeContent.ShowAction()],
@@ -22,7 +25,11 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.action.onClicked.addListener((tab) => {
     const pathParams = new URL(tab.url).pathname.split('/');
-    chrome.tabs.create({ url: `https://www.randoplan.com?rwgpsRoute=${pathParams[2]}&stopAfterLoad=true` });
+    if (tab.url.includes('ridewithgps.com')) {
+        chrome.tabs.create({ url: `https://www.randoplan.com?rwgpsRoute=${pathParams[2]}&stopAfterLoad=true` });
+    } else {
+        chrome.tabs.create({ url: `https://www.randoplan.com?strava_route=${pathParams[2]}&stopAfterLoad=true` });
+    }
 });
 
 chrome.runtime.onMessageExternal.addListener(
@@ -30,7 +37,7 @@ chrome.runtime.onMessageExternal.addListener(
         if (request) {
             if (request.message) {
                 if (request.message == "version") {
-                    sendResponse({ version: 1.2 });
+                    sendResponse({ version: 1.3 });
                 }
             }
         }
