370efe2e90f6c41c6222a84c69d5b5e0918a2240
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2024-02-08 09:34:23 -0800

    Approved AppStore version

---
+++ extension/RandoplanShare/RpViewController.swift
@@ -0,0 +1,109 @@
+//
+//  RpViewController.swift
+//  RandoplanShare
+//
+//  Created by BRIAN FEINBERG on 2/5/24.
+//  Copyright © 2024 Randoplan.com. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import SwiftUI
+import Social
+//import SafariServices
+import UniformTypeIdentifiers
+
+class RpViewController: UIViewController {
+    
+    enum routeType : Equatable {
+        case rwgps
+        case strava
+    }
+    
+    private func makeRandoplanUrl(routeId: String, type: routeType) -> URL {
+        return URL(string:"https://www.randoplan.com?" + ((type==routeType.rwgps) ? "rwgpsRoute=":"strava_route=") + routeId + "&stopAfterLoad=true")!
+    }
+    
+    @objc func openURL(_ url: URL) -> Bool {
+        var responder: UIResponder? = self
+        while responder != nil {
+            if let application = responder as? UIApplication {
+                return application.perform(#selector(openURL(_:)), with: url) != nil
+            }
+            responder = responder?.next
+        }
+        return false
+    }
+    
+    private let typeText = UTType.text.identifier
+    private let typeURL = UTType.url.identifier
+    
+    private func handleIncomingText(itemProvider: NSItemProvider) {
+        itemProvider.loadItem(forTypeIdentifier: typeText as String, options: nil) { (item, error) in
+            if let error = error {
+                self.extensionContext?.cancelRequest(withError: error)
+            }
+            
+            if let text = item as? String {
+                do {
+                    // Detect URLs in String
+                    let detector = try NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
+                    let matches = detector.matches(
+                        in: text,
+                        options: [],
+                        range: NSRange(location: 0, length: text.utf16.count)
+                    )
+                    // Get first URL found
+                    if let firstMatch = matches.first, let range = Range(firstMatch.range, in: text) {
+                        let contentText = String(text[range])
+                        let routeId = URL(string: contentText)?.lastPathComponent
+                        let randoplanUrl = self.makeRandoplanUrl(routeId:routeId!,type:routeType.rwgps)
+                        print("Opened RWGPS URL with result " + String(self.openURL(randoplanUrl)))
+//                        let config = SFSafariViewController.Configuration()
+//                        let vc = SFSafariViewController(url: randoplanUrl, configuration: config)
+//                        self.present(vc, animated: true, completion: {() in print("done with sfsafari")})
+}
+                } catch let error {
+                    self.extensionContext?.cancelRequest(withError: error)
+                }
+            }
+            
+            self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+        }
+    }
+    
+    private func handleIncomingURL(itemProvider: NSItemProvider) {
+        itemProvider.loadItem(forTypeIdentifier: typeURL, options: nil) { (item, error) in
+            if let error = error {
+                self.extensionContext?.cancelRequest(withError: error)
+            }
+            
+            if let url = item as? NSURL, let urlString = url.absoluteString {
+                let routeId = URL(string: urlString)?.lastPathComponent
+                let randoplanUrl = self.makeRandoplanUrl(routeId: routeId!, type:routeType.strava)
+                print("Opened Strava URL with result " + String(self.openURL(randoplanUrl)))
+            }
+            self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        guard let extensionItem = extensionContext?.inputItems.first as? NSExtensionItem,
+              let itemProvider = extensionItem.attachments?.first else {
+            self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+            return
+        }
+        
+        if itemProvider.hasItemConformingToTypeIdentifier(typeText) {
+            handleIncomingText(itemProvider: itemProvider)
+            // Check if object is of type URL
+        } else if itemProvider.hasItemConformingToTypeIdentifier(typeURL) {
+            handleIncomingURL(itemProvider: itemProvider)
+        } else {
+            print("Error: No url or text found")
+            self.extensionContext?.completeRequest(returningItems: nil, completionHandler: nil)
+        }
+    }
+}
--- extension/Shared (Extension)/Resources/background.js
+++ extension/Shared (Extension)/Resources/background.js
@@ -4,39 +4,30 @@ browser.action.onClicked.addListener((tab) => {
         chrome.tabs.create({ url: `https://www.randoplan.com?rwgpsRoute=${pathParams[2]}&stopAfterLoad=true` });
     } else if (tab.url.includes('strava.com/routes') && tab.url.match(".*[0-9]+$")) {
         chrome.tabs.create({ url: `https://www.randoplan.com?strava_route=${pathParams[2]}&stopAfterLoad=true` });
+    } else {
+        browser.action.disable(tabId)
     }
 });
-
-//browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-//    if (request.command === "load") {
-//        const pathParams = new URL(request.url).pathname.split('/');
-//        if (request.url.includes('ridewithgps.com/routes')) {
-//            browser.tabs.create({ url: `https://www.randoplan.com?rwgpsRoute=${pathParams[2]}&stopAfterLoad=true` });
-//        } else if (request.url.includes('strava.com/routes')){
-//            browser.tabs.create({ url: `https://www.randoplan.com?strava_route=${pathParams[2]}&stopAfterLoad=true` });
-//        }
-//        sendResponse({load:"success"})
-//    }
-//});
 
 browser.runtime.onMessageExternal.addListener(
       function (request, sender, sendResponse) {
           if (request) {
               if (request.message) {
-//                  console.log(`message from ${JSON.stringify(sender)}`)
                   if (request.message == "version") {
                       sendResponse({ version: 1.4 });
                   }
               }
           } else {
-//              if (sendResponse)
-//                  sendResponse({"Undefined request"})
-//              else
-                  return true
+              return true
           }
 });
 
-//browser.runtime.onConnectExternal.addListener(function(port) {
-//    console.log(`Connection request received! for port ${JSON.stringify(port)}`);
-//    port.postMessage({content:"Hello from the extension"})
-//});
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.url.includes('ridewithgps.com/routes') && tab.url.match(".*[0-9]+$")) {
+        return
+    } else if (tab.url.includes('strava.com/routes') && tab.url.match(".*[0-9]+$")) {
+        return
+    } else {
+        browser.action.disable(tabId)
+    }
+});
