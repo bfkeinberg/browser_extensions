0257e9afa2b0f8110ecd0f59eb9bf5217abf34b7
Brian Feinberg <bfkeinberg@users.noreply.github.com>
2025-03-15 17:04:53 -0700

    handle route urls with optional trailing slash

--- chrome_rp_extension/background.js
+++ chrome_rp_extension/background.js
@@ -25,9 +25,9 @@ chrome.runtime.onInstalled.addListener(() => {
 
 chrome.action.onClicked.addListener((tab) => {
     const pathParams = new URL(tab.url).pathname.split('/');
-    if (tab.url.includes('ridewithgps.com/routes') && tab.url.match(".*[0-9]+$")) {
+    if (tab.url.includes('ridewithgps.com/routes') && tab.url.match(".*[0-9]+/?$")) {
         chrome.tabs.create({ url: `https://www.randoplan.com?rwgpsRoute=${pathParams[2]}&stopAfterLoad=true` });
-    } else if (tab.url.includes('strava.com/routes') && tab.url.match(".*[0-9]+$")) {
+    } else if (tab.url.includes('strava.com/routes') && tab.url.match(".*[0-9]+/?$")) {
         chrome.tabs.create({ url: `https://www.randoplan.com?strava_route=${pathParams[2]}&stopAfterLoad=true` });
     }
 });
@@ -37,7 +37,7 @@ chrome.runtime.onMessageExternal.addListener(
         if (request) {
             if (request.message) {
                 if (request.message == "version") {
-                    sendResponse({ version: 1.4 });
+                    sendResponse({ version: '1.4.1' });
                 }
             }
         }
